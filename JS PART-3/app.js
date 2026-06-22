4eb8e1a1aaab7fa88f88c11992452ac5f5bc4699
Vedant Pawar <vedantpofficial@gmail.com>
2026-06-22 18:01:58 +0530

    auto update: 2026-06-22 18:01:58

--- JS PART-3/app.js
+++ JS PART-3/app.js
@@ -3,3 +3,6 @@ let msg = "    Hello    ";
 // trim() method removes whitespace from both ends of a string
 console.log(msg);
 console.log(msg.trim());
+console.log(msg.toUpperCase().trim());
+console.log(msg.toLowerCase().trim());
+
